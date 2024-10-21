323a40c119c3551779e8a87bc7e1b295f289423c
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2024-10-21 16:40:35 +0000

    Improve audio file path in get_audio.py and main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,18 @@ from transcript import get_audio_transcript
 
 app = FastAPI()
 
-filename = os.path.dirname(__file__) + "/audio/voz-docente-005.m4a"
-transcript, final_transcript = get_audio_transcript(filename)
+# filename = os.path.dirname(__file__) + "/audio/voz-docente-005.m4a"
+# transcript, final_transcript = get_audio_transcript(filename)
 
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
-@app.get("/")
-def read_root():
+@app.get("/audio/{audio_id}")
+def read_root(audio_id: str):
+    filename = os.path.dirname(__file__) + f"/audio/{audio_id}.m4a"
+    transcript, final_transcript = get_audio_transcript(filename)
+    
     return {
         "original_transcription": transcript,
         "final_transcription": final_transcript,

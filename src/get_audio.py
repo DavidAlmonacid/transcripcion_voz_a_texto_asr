4d825ccd1b9577e0cd6c258422931368fedfbd3f
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2024-10-24 02:55:10 +0000

    Refactorizar función get_audio para mejorar la ruta del archivo de audio

--- src/get_audio.py
+++ src/get_audio.py
@@ -9,13 +9,14 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
 
-res = supabase.storage.from_("audios").download()
 
-# def get_audio(destination_file: str, source_file: str):
-#     with open(destination_file, "wb+") as f:
-#         res = supabase.storage.from_("bucket_name").download(source_file)
-#         f.write(res)
-#         f.close()
+def get_audio(source_file: str):
+    destination_file = f"audio/{source_file}.m4a"
+
+    with open(destination_file, "wb+") as f:
+        res = supabase.storage.from_("audios").get_public_url(source_file)
+        f.write(res)
+        f.close()
 
 
 # get_audio("/audio/voz-docente.mp3", "voz-docente.mp3")

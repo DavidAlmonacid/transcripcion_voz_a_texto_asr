410324df44d6d4cfe4d33fb5563619fde47e6410
David Almonacid <david.almonacidf@gmail.com>
2024-10-31 19:50:43 -0500

    Increase original transcript lenght

--- src/transcript.py
+++ src/transcript.py
@@ -25,7 +25,7 @@ def get_audio_transcript(filename: str):
             file.close()
 
             synthesis = (
-                transcript if len(transcript) <= 80 else get_synthesis_text(transcript)
+                transcript if len(transcript) <= 90 else get_synthesis_text(transcript)
             )
 
             return (transcript, synthesis)
